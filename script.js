4d81dfe4d8cd0552beab465c0e5d7b164a660d2c
timothyhoover <timmchoover@gmail.com>
2021-01-16 13:55:24 -0600

    css 1st round edit

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+// Getting all required elements
+const startBtn = document.querySelector('.start-btn button');
+const infoBox = document.querySelector('.info-box');
+const exitBtn = document.querySelector('.buttons .quit');
+const continueBtn = document.querySelector('.buttons .restart');
 
+// If Start Quiz Button Clicked
+startBtnd.onclick = () => {
+  infoBox.classList.add('');
+};
