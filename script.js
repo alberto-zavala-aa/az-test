3a967376e5000bb69a5972c8722d67023be98e3e
timothyhoover <timmchoover@gmail.com>
2021-01-16 15:17:31 -0600

    commit js functionality and new question js file

--- script.js
+++ script.js
@@ -3,8 +3,20 @@ const startBtn = document.querySelector('.start-btn button');
 const infoBox = document.querySelector('.info-box');
 const exitBtn = document.querySelector('.buttons .quit');
 const continueBtn = document.querySelector('.buttons .restart');
+const quizBox = document.querySelector('.quiz-box');
 
 // If Start Quiz Button Clicked
-startBtnd.onclick = () => {
-  infoBox.classList.add('');
+startBtn.onclick = () => {
+  infoBox.classList.add('activeInfo'); // show the info box
+};
+
+// If Exit Button Clicked
+exitBtn.onclick = () => {
+  infoBox.classList.remove('activeInfo'); // hide the info box
+};
+
+// Continue Button Clicked
+continueBtn.onclick = () => {
+  infoBox.classList.remove('activeInfo'); // hide the info box
+  quizBox.classList.add('activeQuiz'); // show the quiz box
 };
